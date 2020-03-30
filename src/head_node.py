cbab70c10f3a657f13b5a24bcb8e8f69966f4076
Keegan Neave <keegan.neave@outlook.com>
2020-03-30 14:47:17 +0000

    tweaked limits and node names

--- src/head_node.py
+++ src/head_node.py
@@ -22,19 +22,22 @@ def callback(data):
 def setservos(pan, tilt):
     global pan_value, tilt_value
 
-    pan_max = 35
-    tilt_max = 35
+    pan_max = 65
+
+    # Positive is back, negative forward, due to servo orientation
+    tilt_backward_max = 25
+    tilt_forward_max = -55
 
     new_pan = pan_value + pan
     if(abs(new_pan) < pan_max):
         pan_value = new_pan
 
     new_tilt = tilt_value + tilt
-    if(abs(new_tilt) < tilt_max):
-        tilt_value
+    if(tilt_backward_max >= new_tilt >= tilt_forward_max):
+        tilt_value = new_tilt
 
-    redboard.servo21(pan_value)
-    redboard.servo22(tilt_value)
+    redboard.servo22(pan_value)
+    redboard.servo21(tilt_value)
 
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
